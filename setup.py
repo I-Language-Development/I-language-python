7d464b44859ea07bfee39003237cc8d298e74b06
ElBe <elbe.dev.plaq@gmail.com>
2023-03-20 20:46:27 +0100

    Update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='I-Language',
+    version='0.1.3',
+    packages=[''],
+    url='https://github.com/I-Language-Development/I-language',
+    license='MIT',
+    author='I Language Development',
+    author_email='',
+    description='The I programming language is a high level programming language written in pure python. '
+)
