6611180c28a31e3e120a7462545c1da8a8380519
ElBe <elbe.dev.plaq@gmail.com>
2023-04-17 13:21:33 +0200

    Added two more nodes

--- ilanguage/Main/_ast.py
+++ ilanguage/Main/_ast.py
@@ -50,10 +50,11 @@ from typing import (
 # BASE NODE #
 #############
 
+
 @dataclass
 class Node:
     """Base class for all AST nodes."""
-    
+
     name: str
     type: str
     value: Any
@@ -62,4 +63,57 @@ class Node:
     arguments: Optional[Dict[str, str]] = None
 
     def __lt__(self: Node, compare_to: Node) -> bool:
-        return compare_to.level < self.level  # This is supposed to be like this, the smaller the level, the higher it is in the tree
+        return (
+            compare_to.level < self.level
+        )  # This is supposed to be like this, the smaller the level, the higher it is in the tree
+
+
+################
+# BASE PROGRAM #
+################
+
+
+@dataclass(init=False)
+class BaseProgram(Node):
+    """Base program node."""
+
+    def __init__(
+        self,
+        name: str,
+        below: List[Optional[Node]],
+        arguments: Optional[Dict[str, str]] = None,
+    ) -> None:
+        self.name = name
+        self.below = below
+        self.arguments = arguments
+
+        super().__init__(name, "Program", None, 0, below, arguments)
+
+    def __lt__(self: BaseProgram, compare_to: Node) -> bool:
+        return False
+
+
+##########
+# IMPORT #
+##########
+
+
+@dataclass(init=False)
+class Import(Node):
+    """Import node."""
+
+    def __init__(
+        self,
+        name: str,
+        level: int,
+        import_selectors: Optional[List[Optional[Node]]] = None,
+        arguments: Optional[Dict[str, str]] = None,
+    ) -> None:
+        self.name = name
+        self.level = level
+        self.import_selectors: List[Optional[Node]] = (
+            import_selectors if import_selectors is not None else []
+        )
+        self.arguments = arguments
+
+        super().__init__(name, "Import", None, level, self.import_selectors, arguments)
