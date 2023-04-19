09b08b805af3179a4453fcab9c73bd38357e52d3
ElBe-Plaq <90863907+ElBe-Plaq@users.noreply.github.com>
2023-04-19 09:04:35 +0200

    Added more things to options

--- ilanguage/Main/options.py
+++ ilanguage/Main/options.py
@@ -28,23 +28,67 @@ DEALINGS IN THE SOFTWARE.
 # IMPORTS #
 ###########
 
+from dataclasses import dataclass, field
 from typing import (
     Any,
     Dict,
     List,
     Union,
+    Tuple,
 )
+
+
+##########
+# OPTION #
+##########
+
+
+@dataclass
+class Option:
+    """Creates a new option."""
+
+    name: str
+    value: Union[bool, str, List[Any], Tuple[Any]]
+    frozen: bool = field(default=False)
+
+
+#########################
+# OPTION SPECIFIC TYPES #
+#########################
+
+
+@dataclass(init=False, repr=False)
+class Version(Option):
+    """Represents a version number."""
+
+    def __init__(self, major: int, minor: int, patch: int, release_type: str = "release") -> None:
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+        self.release_type = release_type
+
+        super().__init__("version", (major, minor, patch, release_type), True)
+        
+    def __repr__(self) -> str:
+        return f"v{self.major}.{self.minor}.{self.patch}{'-' + self.release_type if self.release_type != 'release' else ''}"
+    
+    def __str__(self) -> str:
+        return repr(self)
+
 
 ###########
 # OPTIONS #
 ###########
 
-options: Dict[str, Union[bool, str, List[Any]]] = {
+options: Dict[str, Option] = {
     # Keep ordered
-    "enable_all_experimental_features": False,  # Enables all experimental features.
-    "enabled_experimental_features": [          # Enables listed experimental features.
+    "enable_all_experimental_features": Option(
+        "enable_all_experimental_features", False
+    ),  # Enables all experimental features.
+    "enabled_experimental_features": [  # Enables listed experimental features.
         # Possible values: "semicolon_not_required"
     ],
-    "exit_zero": False,                         # Exits with exit code zero, even if errors occur.
-                                                # When an exit using the PyAPI is called, the exit can have non-zero exit codes tho.
+    "exit_zero": False,  # Exits with exit code zero, even if errors occur.
+    # When an exit using the PyAPI is called, the exit can have non-zero exit codes tho.
+    "version": Version(0, 0, 10, "dev-6"),  # Version of the language
 }
